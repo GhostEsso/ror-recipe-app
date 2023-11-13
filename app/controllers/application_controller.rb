b45643bb9970f5a153b9eee8e91b58d643c1daf3
GhostEsso <lilipitaham@gmail.com>
2023-11-13 18:26:25 +0000

    linters error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
