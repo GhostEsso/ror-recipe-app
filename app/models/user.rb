b78f319fe580c0f2f68b9577063b7ddda9541763
GhostEsso <lilipitaham@gmail.com>
2023-11-13 18:10:24 +0000

    application_controller updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthale
+    devise :database_authenticatable, :registrable,
+            :recoverable, :rememberable, :validatable, :confirmable
 end
