d8e45cff02b1d7a793247bc3c05e37ef1c4b753d
GhostEsso <lilipitaham@gmail.com>
2023-11-13 20:14:33 +0000

    ability rules added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,12 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
+      can :read, :all
+      return unless user.present?
+
+      can :manage, Recipe, user_id: user.id
+      
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
