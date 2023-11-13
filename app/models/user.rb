127b492a0aa3d222b46ea9202488cdb529c7f506
GhostEsso <lilipitaham@gmail.com>
2023-11-13 18:25:27 +0000

    devise added to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthale
-    devise :database_authenticatable, :registrable,
-            :recoverable, :rememberable, :validatable, :confirmable
-end
+    # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable, :confirmable
+  end
+  
