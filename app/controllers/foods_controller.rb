4f4f96c00231b2750ba9832d26d4314da63c2c72
GhostEsso <lilipitaham@gmail.com>
2023-11-14 10:45:56 +0000

    files updated

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,32 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foodlist = Food.all
+  end
 
-  def show; end
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Successfully destroyed foodlist.'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,8 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_food, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
 end
