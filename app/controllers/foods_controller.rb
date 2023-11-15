dd1a33237c4d31081b16d8e3a36f719321eab5f9
Javier <javidx@gmail.com>
2023-11-15 15:10:14 +0200

    Fixed lint issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -28,7 +28,7 @@ class FoodsController < ApplicationController
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
-  
+
   def set_food
     @food = Food.find(params[:id])
   end
